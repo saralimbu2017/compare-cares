9d403c6b04454406fc0f82c018e18f7257421146
irenema <irene.ma@outlook.com>
2019-05-22 23:48:54 +1000

    details.erb

--- main.rb
+++ main.rb
@@ -126,7 +126,17 @@ get '/contact' do
   erb :contact
 end
 
-get '/details' do
+get '/details/:id' do
+  
+  # How to turn the sql into ActiveRecord?
+  sql = "select b.name, b.location, b.cost, c.name
+  from aged_care_services a
+  join aged_cares b          on a.aged_care_id = b.id
+  join services c            on a.service_id = c.id
+  where a.aged_care_id = #{params[:id]}"
+
+  @aged_care_services = ActiveRecord::Base.connection.execute(sql).values
+
   erb :details
 end
 
